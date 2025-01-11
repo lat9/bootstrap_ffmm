9d0706c24fe7db064ee1026864a0b150523d2d6e
lat9 <lat9@vinosdefrutastropicales.com>
2025-01-11 09:38:59 -0500

    Update ZC plugin ID in readmes and manifest

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ This `encapsulated` plugin provides a means for a Zen Cart admin to create a mul
 
 **Support Thread:** https://www.zen-cart.com/showthread.php?230538-Flexible-Footer-Menu-Multilingual-for-Bootstrap&p=1405247
 
-**Download Link:** https://www.zen-cart.com/downloads.php?do=file&id=TBD
+**Download Link:** https://www.zen-cart.com/downloads.php?do=file&id=2397
 
 Refer to the [readme.html](https://htmlpreview.github.io/?https://github.com/lat9/bootstrap_ffmm/blob/main/readme.html) for additional information.
--- zc_plugins/BFFM/v2.0.0/manifest.php
+++ zc_plugins/BFFM/v2.0.0/manifest.php
@@ -4,7 +4,7 @@ return [
     'pluginName' => 'Bootstrap: Flexible Footer Menu Multilingual',
     'pluginDescription' => 'This plugin enables a Zen Cart admin to configure a multi-column multilingual footer menu for the Bootstrap template.',
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
-    'pluginId' => 0, // ID from Zen Cart forum
+    'pluginId' => 2397, // ID from Zen Cart forum
     'zcVersions' => [],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => '', // url
